cdc0ce5525ce02393b89646886a6ec14203e2a59
Brayden Bowler <braybowler1995@gmail.com>
2025-07-09 16:19:09 -0400

    add some text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,11 +5,27 @@ import ElapsedClock from '@/components/ElapsedClock.vue'
 <template>
   <main class="xs:flex xs:flex-col xs:gap-2 sm:gap-4 sm:flex sm:flex-row">
     <section class="xs:w-full xs:h-1/2 sm:w-1/2 sm:h-full">
-<!--      <h2 class="xs:text-base sm:text-xl font-bold text-[#ff5a13]">Hi, I'm Brayden...</h2>-->
-      <ElapsedClock />
+      <h2 class="xs:text-base xs:mb-1 sm:text-xl sm:mb-2 font-extrabold text-[#ff5a13]">Hi, I'm Brayden...</h2>
+
+      <h3 class="xs:text-sm xs:mb-1 sm:text-lg sm:mb-2 font-bold text-neutral-400">
+        I'm an enthusiastic full-stack developer with a fresh perspective on software development.
+      </h3>
+      <h3 class="xs:text-sm xs:mb-1 sm:text-lg sm:mb-2 font-bold text-neutral-400">
+        I grew up watching the Internet evolve. Now, I'm lucky to help contribute to that evolution on a daily basis.
+      </h3>
+
+      <p>
+        Founding member of <a href="https://bkgsoftware.com">BKG Software</a>.
+      </p>
+      <p>
+        Creator of <a href="https://apps.apple.com/ca/app/wedge-matrix/id6744891049">Wedge Matrix</a>.
+      </p>
+      <p>
+        Full-time developer at <a href="https://vehikl.com">Vehikl</a>.
+      </p>
     </section>
-<!--    <section class="xs:w-full xs:h-1/2 sm:w-1/2 sm:h-full">-->
-<!--      Right or Bottom-->
+<!--    <section class="xs:w-full xs:h-1/2 sm:w-1/2 sm:h-full flex items-baseline">-->
+<!--      <ElapsedClock />-->
 <!--    </section>-->
   </main>
 </template>
